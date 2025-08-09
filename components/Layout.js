3101741a370c9ab98e4aecc92464e60a3ca61b52
danyaffa <leffleryd@gmail.com>
2025-08-09 19:45:28 +1000

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Header from './Header';
 import Footer from './Footer';
 
-export default function Layout({ children }) {
+export default function AppLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
