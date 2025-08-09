9b1d89b1ad832d8577b3587d3e9bb5e282ee62b9
danyaffa <leffleryd@gmail.com>
2025-08-09 18:30:02 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,12 @@
-import "../styles/globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import '../styles/globals.css';
+import Layout from '../components/Layout';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Header />
-      <main>
-        <div className="container">
-          <Component {...pageProps} />
-        </div>
-      </main>
-      <Footer />
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
+
+export default MyApp;
