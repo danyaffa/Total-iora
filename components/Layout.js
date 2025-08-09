fe7da606069427d32a8c2c95052dd3eead1b405c
danyaffa <leffleryd@gmail.com>
2025-08-09 18:29:19 +1000

    Create Layout.js

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Footer from './Footer';
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 sm:px-6 py-8 sm:py-12">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
