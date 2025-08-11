588397ea300b7a634cd695aaf57b9209ed49fc51
danyaffa <leffleryd@gmail.com>
2025-08-12 06:52:01 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 // FILE: /pages/_app.js
-import "../styles/globals.css";
+import "@/styles/globals.css";
+import AccessGate from "@/components/AccessGate";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// NOTE: This wraps your entire app with the gate overlay.
+// Your /pages/index.js stays EXACTLY as you wrote it.
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <AccessGate />
+      <Component {...pageProps} />
+    </>
+  );
 }
