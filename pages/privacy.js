8ee700f38761f4be480a5fd7108faab4476bf68e
danyaffa <leffleryd@gmail.com>
2025-08-12 10:43:10 +1000

    Update privacy.js

--- pages/privacy.js
+++ pages/privacy.js
@@ -2,7 +2,7 @@ export default function Privacy(){
   return (
     <div className="prose">
       <h2>Privacy Policy</h2>
-      <p>We respect your privacy. Notes placed on the Aura Wall are private and not shared with other users nor read by humans.
+      <p>We respect your privacy. Notes placed on the Total-iora pages are private and not shared with others.
       We store minimal data required to operate and may delete inactive content after 12 months.</p>
       <p>Voice sessions are processed to generate a reflection and are not stored by default.</p>
     </div>
