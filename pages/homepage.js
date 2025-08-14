08af0d3f1d285a042b429b8ead31fc1115b04a5b
danyaffa <leffleryd@gmail.com>
2025-08-14 20:05:10 +1000

    Update homepage.js

--- pages/homepage.js
+++ pages/homepage.js
@@ -46,18 +46,14 @@ export async function getServerSideProps(ctx) {
 
 function FaithIcon({ faith }) {
   if (faith === "Muslim") {
-    // Crescent (with evenodd fillRule to ensure correct rendering)
+    // Crescent (using a new, more reliable SVG path)
     return (
       <svg
         className="faith-icon gold"
-        viewBox="0 0 64 64"
+        viewBox="0 0 52 52"
         aria-label="Muslim Crescent"
       >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M39.5 6a22.5 22.5 0 1 0 0 52a20 20 0 1 1 0-52z"
-        />
+        <path d="M32 2 C18.7 2 8 12.7 8 26 C8 39.3 18.7 50 32 50 C33.7 50 35.4 49.8 37 49.5 C26.5 47.5 19 37.9 19 26 C19 14.1 26.5 4.5 37 2.5 C35.4 2.2 33.7 2 32 2Z" />
       </svg>
     );
   }
