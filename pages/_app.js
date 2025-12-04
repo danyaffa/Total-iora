ac70cb30a07f6c207735eb7d6fd0634204fd530b
Dan Leffler <leffleryd@gmail.com>
2025-12-04 17:24:18 +1100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 // FILE: /pages/_app.js
-import "@/styles/globals.css";
 
-// REMOVE the AccessGate import and wrapper.
-// Keep your app exactly as-is so visitors can read everything pre-login.
+import "@/styles/globals.css";
+import ReviewWidget from "../components/ReviewWidget";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      {/* Floating 4.9/5 pill, bottom-right on every page */}
+      <ReviewWidget />
+    </>
+  );
 }
